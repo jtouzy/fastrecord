cbb5e5fc3fa5635bdf5e5cff43682c2a7d9a313f
Jérémy TOUZY <jtouzy2@gmail.com>
2016-12-20 18:36:54 +0100

    [update] QueryContext : Chain instead of List for FromContext

--- src/main/java/com/jtouzy/fastrecord/statements/context/BaseQueryContext.java
+++ src/main/java/com/jtouzy/fastrecord/statements/context/BaseQueryContext.java
@@ -1,15 +1,17 @@
 package com.jtouzy.fastrecord.statements.context;
 
+import com.jtouzy.fastrecord.utils.Chain;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseQueryContext implements QueryContext {
     private final List<AliasExpressionContext> columnContextList;
-    private final List<QueryFromContext> fromContextList;
+    private final Chain<QueryFromContext,JoinOperator> fromContextChain;
 
     public BaseQueryContext() {
         this.columnContextList = new ArrayList<>();
-        this.fromContextList = new ArrayList<>();
+        this.fromContextChain = new Chain<>();
     }
 
     @Override
@@ -18,8 +20,8 @@ public class BaseQueryContext implements QueryContext {
     }
 
     @Override
-    public List<QueryFromContext> getFromContextList() {
-        return fromContextList;
+    public Chain<QueryFromContext,JoinOperator> getFromContextChain() {
+        return fromContextChain;
     }
 
     @Override
@@ -29,6 +31,11 @@ public class BaseQueryContext implements QueryContext {
 
     @Override
     public void addFromContext(QueryFromContext fromContext) {
-        this.fromContextList.add(fromContext);
+        this.fromContextChain.addFirst(fromContext);
+    }
+
+    @Override
+    public void addFromContext(JoinOperator operator, QueryFromContext fromContext) {
+        this.fromContextChain.add(operator, fromContext);
     }
 }
--- src/main/java/com/jtouzy/fastrecord/statements/context/QueryContext.java
+++ src/main/java/com/jtouzy/fastrecord/statements/context/QueryContext.java
@@ -1,10 +1,13 @@
 package com.jtouzy.fastrecord.statements.context;
 
+import com.jtouzy.fastrecord.utils.Chain;
+
 import java.util.List;
 
 public interface QueryContext extends ExpressionContext, QueryFromContext {
     void addColumnContext(AliasExpressionContext expressionContext);
     void addFromContext(QueryFromContext fromContext);
+    void addFromContext(JoinOperator operator, QueryFromContext fromContext);
     List<AliasExpressionContext> getColumnContextList();
-    List<QueryFromContext> getFromContextList();
+    Chain<QueryFromContext,JoinOperator> getFromContextChain();
 }
