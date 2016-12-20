daf54bf06549c7a9ab21537c6ffae64b70b34602
Jérémy TOUZY <jtouzy2@gmail.com>
2016-12-20 18:53:42 +0100

    [add] TableAliasWriter + QueryWriter From implementation

--- src/main/java/com/jtouzy/fastrecord/statements/writers/BaseQueryWriter.java
+++ src/main/java/com/jtouzy/fastrecord/statements/writers/BaseQueryWriter.java
@@ -1,8 +1,11 @@
 package com.jtouzy.fastrecord.statements.writers;
 
 import com.jtouzy.fastrecord.statements.context.AliasExpressionContext;
+import com.jtouzy.fastrecord.statements.context.JoinOperator;
 import com.jtouzy.fastrecord.statements.context.QueryContext;
+import com.jtouzy.fastrecord.statements.context.QueryFromContext;
 import com.jtouzy.fastrecord.statements.processing.DbReadyStatementMetadata;
+import com.jtouzy.fastrecord.utils.Chain;
 
 import java.util.Iterator;
 
@@ -14,11 +17,12 @@ public class BaseQueryWriter<T extends QueryContext> extends AbstractWriter<T> {
     @Override
     public DbReadyStatementMetadata write() {
         super.write();
-        appendQueryColumns();
+        appendColumns();
+        appendFrom();
         return buildMetadata();
     }
 
-    private void appendQueryColumns() {
+    private void appendColumns() {
         getSqlString().append("SELECT ");
         // TODO contextValidation for no columns
         Iterator<AliasExpressionContext> it = getContext().getColumnContextList().iterator();
@@ -28,8 +32,18 @@ public class BaseQueryWriter<T extends QueryContext> extends AbstractWriter<T> {
                 getSqlString().append(", ");
             }
         }
+    }
+
+    private void appendFrom() {
         getSqlString().append(" FROM ");
-        // TODO append FROM
-        // TODO append WHERE
+        // TODO contextValidation for no from
+        Iterator<Chain.ChainItemWrapper<QueryFromContext,JoinOperator>> itf =
+                getContext().getFromContextChain().iterator();
+        while (itf.hasNext()) {
+            mergeWriter(itf.next().getItem());
+            if (itf.hasNext()) {
+                getSqlString().append(", ");
+            }
+        }
     }
 }
