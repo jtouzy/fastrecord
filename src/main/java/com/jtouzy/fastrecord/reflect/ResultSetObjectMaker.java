4c498754e9ed18f58cd6381310d9329c0d6011b9
Jérémy TOUZY <jtouzy2@gmail.com>
2016-12-30 09:45:43 +0100

    [refactor] Object creation algorithm rewrite

--- src/main/java/com/jtouzy/fastrecord/reflect/ResultSetObjectMaker.java
+++ src/main/java/com/jtouzy/fastrecord/reflect/ResultSetObjectMaker.java
@@ -14,9 +14,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ResultSetObjectMaker<T> {
     private static final Logger logger = LoggerFactory.getLogger(ResultSetObjectMaker.class);
@@ -25,6 +25,9 @@ public class ResultSetObjectMaker<T> {
     private final ResultSet resultSet;
     private final Map<String,EntityDescriptor> entityDescriptorsByAlias;
     private final Table<String,ColumnDescriptor,String> columnDescriptorAliasMapping;
+    private final Map<String,Object> currentRowValues;
+    private final Table<Object,String,Object> currentRowInstanceCache;
+    private final Map<String,Object> currentRowAliasInstanceMapping;
 
     public ResultSetObjectMaker(FastRecordConfiguration configuration, QueryExpression queryContext,
                                 Map<String,EntityDescriptor> entityDescriptorsByAlias,
@@ -35,6 +38,9 @@ public class ResultSetObjectMaker<T> {
         this.entityDescriptorsByAlias = entityDescriptorsByAlias;
         this.columnDescriptorAliasMapping = columnDescriptorAliasMapping;
         this.resultSet = resultSet;
+        currentRowValues = new HashMap<>();
+        currentRowInstanceCache = HashBasedTable.create();
+        currentRowAliasInstanceMapping = new HashMap<>();
     }
 
     @SuppressWarnings("unchecked")
@@ -42,23 +48,20 @@ public class ResultSetObjectMaker<T> {
         List<T> results = new ArrayList<>();
         try {
             String columnLabel;
-            Map<String,Object> rowValuesMap;
-
             logger.debug("Start creating result objects...");
             String mainTableAlias = queryContext.getMainTargetExpression().getAlias();
             while (resultSet.next()) {
-                // Stores all the ResultSet row values in a HashMap to avoid multiple
-                // loops over the resultSet columns during the object creation
-                rowValuesMap = new HashMap<>();
+                currentRowValues.clear();
+                currentRowInstanceCache.clear();
                 for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i ++) {
                     // Access to the column label : tableAlias0$$tableColumn
                     columnLabel = resultSet.getMetaData().getColumnLabel(i);
                     // Stores the value in the value Map
-                    rowValuesMap.put(columnLabel, resultSet.getObject(columnLabel));
+                    currentRowValues.put(columnLabel, resultSet.getObject(columnLabel));
                 }
                 // Starts the object creation for the main object
                 logger.debug("Start creating result object for row...");
-                results.add((T)createObjectFromResultSetRow(mainTableAlias, rowValuesMap));
+                results.add((T)createObjectFromAlias(mainTableAlias));
             }
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException | SQLException ex) {
             // TODO IllegalArgumentException if argument type mismatch + Other exceptions?
@@ -67,57 +70,71 @@ public class ResultSetObjectMaker<T> {
         return results;
     }
 
-    private Object createObjectFromResultSetRow(String tableAlias, Map<String,Object> values)
-    throws InvocationTargetException, InstantiationException, IllegalAccessException, SQLException {
-        logger.debug("Start creating object with table alias [{}]", tableAlias);
+    private Object createObjectFromAlias(String tableAlias)
+    throws InvocationTargetException, IllegalAccessException, InstantiationException {
         EntityDescriptor entityDescriptor = entityDescriptorsByAlias.get(tableAlias);
-        Object instance = createObjectFromClass(entityDescriptor.getClazz());
-        Map<String,Object> resultSetValuesForEntity = new LinkedHashMap<>();
-        values.entrySet().stream().filter(e -> e.getKey().startsWith(tableAlias)).forEach(e ->
-            resultSetValuesForEntity.put(
-                    e.getKey().substring(e.getKey().indexOf(configuration.getColumnAliasSeparator()) +
-                            configuration.getColumnAliasSeparator().length()),
-                    e.getValue())
-        );
-        logger.debug("Values to set on the new object [{}]", resultSetValuesForEntity);
-        ColumnDescriptor columnDescriptor;
-        String relatedAlias;
-        Table<Object,String,Object> instanceCache = HashBasedTable.create();
-        for (Map.Entry<String,Object> valueEntry : resultSetValuesForEntity.entrySet()) {
-            // TODO safe checking of optional columnDescriptor
-            columnDescriptor = entityDescriptor.getColumnDescriptorByColumn(valueEntry.getKey()).get();
-            relatedAlias = columnDescriptorAliasMapping.get(tableAlias, columnDescriptor);
-            invokeSetter(relatedAlias, valueEntry, values, instanceCache, instance, columnDescriptor, valueEntry.getValue());
+        Object instance = currentRowAliasInstanceMapping.get(tableAlias);
+        if (instance == null) {
+            instance = newInstance(entityDescriptor.getClazz());
+        }
+        StringBuilder columnName = new StringBuilder();
+        Object valueToSet;
+        for (ColumnDescriptor columnDescriptor : entityDescriptor.getColumnDescriptors()) {
+            columnName.delete(0, columnName.length());
+            columnName.append(tableAlias)
+                      .append(configuration.getColumnAliasSeparator())
+                      .append(columnDescriptor.getColumnName());
+            valueToSet = currentRowValues.get(columnName.toString());
+            if (valueToSet != null) {
+                setPropertyValue(tableAlias, instance, columnDescriptor, valueToSet);
+            }
+        }
+        if (queryContext.getTargetJoinList().size() > 0) {
+            List<String> joinedAliases = queryContext.getTargetJoinList().stream()
+                    .filter(j -> j.getFirstTargetExpression().getAlias().equals(tableAlias))
+                    .map(j -> j.getJoinTargetExpression().getAlias()).collect(Collectors.toList());
+            for (String joinedAlias : joinedAliases) {
+                createObjectFromAlias(joinedAlias);
+            }
         }
         return instance;
     }
 
-    private Object createObjectFromClass(Class fromClass)
-    throws InstantiationException, IllegalAccessException {
-        return fromClass.newInstance();
+    private void setPropertyValue(String tableAlias, Object instance, ColumnDescriptor columnDescriptor, Object valueToSet)
+    throws InvocationTargetException, IllegalAccessException, InstantiationException {
+        if (columnDescriptor.isRelated()) {
+            Object relatedInstance = currentRowInstanceCache.get(instance, columnDescriptor.getPropertyName());
+            String alias = columnDescriptorAliasMapping.get(tableAlias, columnDescriptor);
+            if (relatedInstance == null) {
+                relatedInstance = newInstance(columnDescriptor.getPropertyType());
+                currentRowInstanceCache.put(instance, columnDescriptor.getPropertyName(), relatedInstance);
+                if (alias != null) {
+                    currentRowAliasInstanceMapping.put(alias, relatedInstance);
+                }
+                invokeSetter(columnDescriptor, instance, relatedInstance);
+            }
+            setPropertyValue(alias != null ? alias : tableAlias, relatedInstance,
+                    columnDescriptor.getRelatedColumn(), valueToSet);
+        } else {
+            invokeSetterWithConversion(columnDescriptor, instance, valueToSet);
+        }
     }
 
-    private void invokeSetter(String tableAlias, Map.Entry<String,Object> valueEntry, Map<String,Object> values, Table<Object,String,Object> instanceCache, Object instance, ColumnDescriptor columnDescriptor, Object value)
-    throws SQLException, InstantiationException, InvocationTargetException, IllegalAccessException {
-        if (columnDescriptor.isRelated()) {
-            Object relatedInstance;
-            relatedInstance = instanceCache.get(instance, columnDescriptor.getPropertyName());
-            if (relatedInstance == null) {
-                if (tableAlias == null) {
-                    relatedInstance = createObjectFromClass(columnDescriptor.getPropertyType());
-                } else {
-                    relatedInstance = createObjectFromResultSetRow(tableAlias, values);
-                }
-                logger.debug("Invoke setter on [{}] (property [{}]) with value [{}]", instance, columnDescriptor.getPropertyName(), relatedInstance);
-                columnDescriptor.getPropertySetter().invoke(instance, relatedInstance);
-                invokeSetter(tableAlias, valueEntry, values, instanceCache, relatedInstance, columnDescriptor.getRelatedColumn(), valueEntry.getValue());
-                instanceCache.put(instance, columnDescriptor.getPropertyName(), relatedInstance);
-            } else {
-                invokeSetter(tableAlias, valueEntry, values, instanceCache, relatedInstance, columnDescriptor.getRelatedColumn(), valueEntry.getValue());
-            }
-        } else {
-            logger.debug("Invoke setter on [{}] (property [{}]) with value [{}]", instance, columnDescriptor.getPropertyName(), value);
-            columnDescriptor.getPropertySetter().invoke(instance, columnDescriptor.getTypeManager().convertToObject(value));
-        }
+    private Object newInstance(Class clazz)
+    throws InstantiationException, IllegalAccessException {
+        logger.debug("New instance of [{}]", clazz);
+        return clazz.newInstance();
+    }
+
+    private void invokeSetterWithConversion(ColumnDescriptor columnDescriptor, Object instance, Object valueToSet)
+    throws InvocationTargetException, IllegalAccessException {
+        invokeSetter(columnDescriptor, instance, columnDescriptor.getTypeManager().convertToObject(valueToSet));
+    }
+
+    private void invokeSetter(ColumnDescriptor columnDescriptor, Object instance, Object valueToSet)
+    throws InvocationTargetException, IllegalAccessException {
+        logger.debug("Invoke on [{}], [{}] with [{}]", instance,
+                columnDescriptor.getPropertySetter().getName(), valueToSet);
+        columnDescriptor.getPropertySetter().invoke(instance, valueToSet);
     }
 }
